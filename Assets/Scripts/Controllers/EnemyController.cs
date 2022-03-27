e603447cb162a16fea7cc5045839052e5aa5f532
Zach Jackson <zsj9993@rit.edu>
2022-03-26 22:24:58 -0600

    Added enemy movement and tracking towards the player within a certain radius

--- Assets/Scripts/Controllers/EnemyController.cs
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -4,15 +4,80 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    public GameObject player;
+    public float enemyViewDistance = 4f;
+    public ContactFilter2D movementFilter;
+    private float distance;
+
+    new Rigidbody2D rigidbody;
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+    //Animator animator;
+    SpriteRenderer spriteRenderer;
+
+    bool canMove = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
+        //animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 enemyMovementDirection = player.transform.position - transform.position;
+        enemyMovementDirection.Normalize();
+        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        if (canMove)
+        {
+            //If the enemy's direction vector is not 0
+            if (enemyMovementDirection != Vector2.zero)
+            {
+                //Try to move in the direction of the player
+                bool success = TryMove(enemyMovementDirection);
+
+                //If unable to move in the player's direction, don't move
+                if (!success)
+                {
+                    return;
+                }
+            }
+        }
+    }
+
+    private bool TryMove(Vector2 enemyMovementDirection)
+    {
+        if (enemyMovementDirection != Vector2.zero)
+        {
+            //Check for potential collisions
+            int count = rigidbody.Cast(enemyMovementDirection, movementFilter, castCollisions, EnemyManager.instance.movement * Time.fixedDeltaTime);
+
+            //If no potential collsions are detected, move the character in the specified direction
+            if (count == 0)
+            {
+                //Check to see if the enemy can see the player
+                if (distance < enemyViewDistance)
+                {
+                    transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, EnemyManager.instance.movement * Time.deltaTime);
+                    //transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
     }
 }
--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -73,7 +73,7 @@ public class PlayerController : MonoBehaviour
             //Check for potential collisions
             int count = rigidbody.Cast(direction, movementFilter, castCollisions, PlayerManager.instance.movement * Time.fixedDeltaTime + collisionOffset);
 
-            //If no potential collisiosn are detected, move the character in the specified direction
+            //If no potential collisions are detected, move the character in the specified direction
             if (count == 0)
             {
                 rigidbody.MovePosition(rigidbody.position + direction * PlayerManager.instance.movement * Time.fixedDeltaTime);
