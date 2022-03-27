e0df0f46b7d3ed2c896453000a5fc8ed4f00adfd
Rhys Stever <rhys@stever.com>
2022-03-27 13:40:06 -0400

    Added a unit class inheritance structure for units, players, and enemies

---
+++ Assets/Scripts/UnitScripts/Player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Unit
+{
+    public int healthRegen;
+    public int stamina;
+    public int staminaRegen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
